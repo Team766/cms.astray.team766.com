e4b7003cc857b2c67e03a8248ccaaac8dfdb5c93
pjztam <pjztam@gmail.com>
2014-11-24 00:35:11 -0800

    update all pages except homepage

--- page.php
+++ page.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 include 'menu.class.php';
 $pageURI = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
@@ -6,11 +6,11 @@ $pageRelativeURI = basename($pageURI);
 $pages = new SimpleXMLElement('content/pages.xml', 0, true);
 
 
-$title = 'Team 766 | 404 - File Not Found';
+$title = '404 File Not Found | Team 766';
 $pageFileURL = 'content/404.html';
 foreach($pages->children() as $page) {
     if ($page->url == $pageRelativeURI) {
-        $title = 'Team 766 | ' . $page->title;
+        $title = $page->title . ' | Team 766';
         $pageFileURL = 'content/' . $page->url . '.html';
     }
 }
