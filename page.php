2016041da73e9e98c5770e9d3b0bc14e12854c05
pjztam <pjztam@gmail.com>
2014-12-04 00:21:11 -0500

    fixed redirect loop 2

--- page.php
+++ page.php
@@ -1,22 +1,19 @@
 <?php 
 include 'menu.class.php';
 $pageURI = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
-$baseDirectory = '/cms.astray.team766.com/';
-$baseDomain = "http://localhost";
 $pageRelativeURI = basename($pageURI);
 $pages = new SimpleXMLElement('content/pages.xml', 0, true);
 
-if (substr_count(str_replace($baseDirectory, "" ,$pageURI),'/') > 0) {
-    header('Location: ' . $baseDomain . $baseDirectory . $pageRelativeURI);
-}
-
-$title = '404 File Not Found | Team 766';
-$pageFileURL = 'content/404.html';
+$title = '';
+$pageFileURL = '';
 foreach($pages->children() as $page) {
     if ($page->url == $pageRelativeURI) {
         $title = $page->title . ' | Team 766';
         $pageFileURL = 'content/' . $page->url . '.html';
     }
+}
+if ($pageFileURL == "") {
+    header('Location: http://dev3.team766.com/404');
 }
 
 $menu = new Menu('mainMenu.xml', $pageRelativeURI);
