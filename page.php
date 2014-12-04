b79b21b2a7300d40ee3ffd313027bc2959fcee54
pjztam <pjztam@gmail.com>
2014-12-04 00:14:18 -0500

    got rid of redirect loop for 404

--- page.php
+++ page.php
@@ -1,10 +1,14 @@
 <?php 
-
 include 'menu.class.php';
 $pageURI = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
-$pageRelativeURI = str_replace('/','',basename($pageURI));
+$baseDirectory = '/cms.astray.team766.com/';
+$baseDomain = "http://localhost";
+$pageRelativeURI = basename($pageURI);
 $pages = new SimpleXMLElement('content/pages.xml', 0, true);
 
+if (substr_count(str_replace($baseDirectory, "" ,$pageURI),'/') > 0) {
+    header('Location: ' . $baseDomain . $baseDirectory . $pageRelativeURI);
+}
 
 $title = '404 File Not Found | Team 766';
 $pageFileURL = 'content/404.html';
